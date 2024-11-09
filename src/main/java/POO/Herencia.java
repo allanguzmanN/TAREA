f0dcab76a7f2893b4b3a1c27f8febeaef2964d59
allangzm18 <guzmanallan48@gmail.com>
2024-11-09 15:17:18 -0600

    Actualizacion

--- src/main/java/POO/Herencia.java
+++ src/main/java/POO/Herencia.java
@@ -4,6 +4,9 @@
 
 package POO;
 
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  *
  * @author allan
@@ -11,10 +14,19 @@ package POO;
 public class Herencia {
 
     public static void main(String[] args) {
-        Perro p = new Perro("Color Negro","SI","SI","SI",4);
-        p.Sonido();
+        Animal p = new Perro("Color Negro","SI","SI","SI",4);
+       //p.Sonido();
         
-        Gato g = new Gato ("Blanco","SI","SI","SI",4);
-        g.Sonido();
+        Animal g = new Gato ("Blanco","SI","SI","SI",4);
+       // g.Sonido();
+        
+        List<Animal> lista = new ArrayList<Animal>();
+        lista.add(p);
+        lista.add(g);
+        lista.add(new Queco("NO","SI","SI","SI",4));
+        for(Animal item : lista){
+            item.Sonido();
+            
+        }
     }
 }
--- src/main/java/POO/Queco.java
+++ src/main/java/POO/Queco.java
@@ -5,7 +5,7 @@
 package POO;
 
 /**
- *
+ 
  * @author allan
  */
 public class Queco extends Animal {
